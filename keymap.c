14e2de82500137b7599f4ba01ffc77aa05ad22b8
SparrowMike <michal.m_wroblewski@hotmail.com>
2024-04-14 00:32:15 +0800

    feat: 🎸 tapping term and mod key test

--- keymap.c
+++ keymap.c
@@ -7,14 +7,6 @@ enum layers {
     _THIRD
 };
 
-enum {
-    TD_LEFT_SHIFT_CAPS,
-};
-
-tap_dance_action_t tap_dance_actions[] = {
-    [TD_LEFT_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-};
-
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LCTL_T(KC_ESC):
@@ -44,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
     LCTL_T(KC_ESC), KC_A, KC_S,  KC_D,  KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    TD(TD_LEFT_SHIFT_CAPS),KC_Z,KC_X, KC_C,KC_V, KC_B,                           KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RALT,
+    MT(MOD_LSFT, KC_CAPS),KC_Z,KC_X, KC_C, KC_V, KC_B,                           KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RALT,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                         KC_LGUI,   MO(_FIRST),  KC_ENT,     KC_SPC,   KC_BSPC, MO(_SECOND)
                                         //`--------------------------'  `--------------------------'
